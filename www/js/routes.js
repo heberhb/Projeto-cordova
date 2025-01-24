55744d65627e164ac238bd5b42ae7a199d5efcf9
heberhb <heber20014@gmail.com>
2025-01-24 00:00:06 -0300

    Estilizando página carrinho

--- www/js/routes.js
+++ www/js/routes.js
@@ -31,7 +31,7 @@ var app = new Framework7({
         },
         pageInit: function (event, page) {
           // fazer algo quando a página for inicializada
-          //app.views.main.router.navigate('/detalhes/')
+          app.views.main.router.navigate('/carrinho/')
           $.getScript('js/index.js');
 
           var swiper = new Swiper(".mySwiper", {
